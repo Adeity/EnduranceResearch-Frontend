417f8a9e9adf8d3f31040e704f27e441114e00b0
Patrik Pavelka <pavelpa2@fit.cvut.cz>
2024-02-07 13:39:20 +0100

    env file empty a error message

--- app/(questionnares)/error/page.js
+++ app/(questionnares)/error/page.js
@@ -1,5 +1,5 @@
 export default function SubmitErrorPage(props) {
-    const questionnairesArray = props.searchParams.message
+    const questionnairesArray = "Nastala nečekaná chyba"
     return (
         <>
             <h3>Nastala chyba. Dejte nám o ní vědět na pavelpa2@fel.cvut.cz</h3>
